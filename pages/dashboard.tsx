e4f30815f7eaddb82e7fbde30309466398cb1bfd
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-01-07 20:12:55 -0500

    adds stylesheet for dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,10 @@
+import styles from '../styles/Dashboard.module.css'
+
 export default function Dashboard(props) {
     return (
         <form>
-            <h1>Form goes here!</h1>
+            <h1>Hello,{props.name}</h1>
+            <h2 className={styles.red}>Form goes here!</h2>
         </form>
     )
 }
