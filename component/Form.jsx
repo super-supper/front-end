c9773686fa822cc79326167f8d9008b4fe9a516a
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-03-12 13:08:46 -0500

    creates css files for create page and form, adds some styles

--- component/Form.jsx
+++ component/Form.jsx
@@ -1,9 +1,31 @@
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import styles from '../styles/form.module.css';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(3),
+        width: '50ch',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+    },
+  }));
+
 const Form = () => {
 
+    const classes = useStyles();
+
     return (
-        <form>
-            
-        </form>
+        <div className={styles.container}>
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+            </form>
+        </div>
     )
 
 }
