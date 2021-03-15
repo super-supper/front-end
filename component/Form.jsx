e337058092e333783fb37ae425453ba565abd419
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-03-15 15:36:30 -0400

    wires up form component to POST endpoint with Formik

--- component/Form.jsx
+++ component/Form.jsx
@@ -1,34 +1,130 @@
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
-import styles from '../styles/form.module.css';
-
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(3),
-        width: '50ch',
-        display: 'flex',
-        flexDirection: 'column',
-      },
-    },
-  }));
+import { Typography } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import axios from "axios";
+import { Formik } from "formik";
+import styles from "../styles/form.module.css";
 
 const Form = () => {
+	return (
+		<div className={styles.container}>
+			<Typography variant="h4" component="h3" gutterBottom>
+				Create A Recipe
+			</Typography>
+			<Formik
+				initialValues={{
+					title: "", //must not be null
+					description: "", //must not be null
+					url: "",
+					ingredients: [
+						{
+							ingredient: "",
+							qty: "",
+						},
+					],
+					steps: [
+						{
+							step_num: 0,
+							step: "",
+						},
+					],
+					tags: [
+						{
+							tag_id: 0,
+						},
+					],
+				}}
+				onSubmit={(values, { setSubmitting }) => {
+					axios
+						.post("https://super-supper.ue.r.appspot.com/recipes", values)
+						.then(() => setSubmitting(false));
+				}}
+			>
+				{({
+					values,
+					handleChange,
+					handleSubmit,
+					isSubmitting,
+					/* and other goodies */
+				}) => (
+					<form className={styles.form} onSubmit={handleSubmit}>
+						<TextField
+							name="title"
+							onChange={handleChange}
+							value={values.title}
+							id="filled-basic"
+							label="Title"
+							variant="filled"
+						/>
 
-    const classes = useStyles();
+						<TextField
+							name="description"
+							onChange={handleChange}
+							value={values.description}
+							id="filled-basic"
+							label="Description"
+							variant="filled"
+						/>
 
-    return (
-        <div className={styles.container}>
-            <form className={classes.root} noValidate autoComplete="off">
-                <TextField id="filled-basic" label="Filled" variant="filled" />
-                <TextField id="filled-basic" label="Filled" variant="filled" />
-                <TextField id="filled-basic" label="Filled" variant="filled" />
-            </form>
-        </div>
-    )
+						<TextField
+							name="url"
+							onChange={handleChange}
+							value={values.url}
+							id="filled-basic"
+							label="URL"
+							variant="filled"
+						/>
+						<div>Ingredients</div>
+						<TextField
+							name="ingredients[0].ingredient"
+							onChange={handleChange}
+							value={values.ingredients[0].ingredient}
+							id="filled-basic"
+							label="Ingredient"
+							variant="filled"
+						/>
+						<TextField
+							name="ingredients[0].qty"
+							onChange={handleChange}
+							value={values.ingredients[0].qty}
+							id="filled-basic"
+							label="Quantity"
+							variant="filled"
+						/>
+						<div>Steps</div>
+						<TextField
+							name="steps[0].step_num"
+							onChange={handleChange}
+							value={values.steps[0].step_num}
+							id="filled-basic"
+							label="Step Number"
+							variant="filled"
+						/>
+						<TextField
+							name="steps[0].step"
+							onChange={handleChange}
+							value={values.steps[0].step}
+							id="filled-basic"
+							label="Step Description"
+							variant="filled"
+						/>
+						<div>Tags</div>
+						<TextField
+							name="tags[0].tag_id"
+							onChange={handleChange}
+							value={values.tags[0].tag_id}
+							id="filled-basic"
+							label="Tag ID"
+							variant="filled"
+						/>
 
-}
+						<button type="submit" disabled={isSubmitting}>
+							Submit
+						</button>
+					</form>
+				)}
+			</Formik>
+		</div>
+	);
+};
 
-export default Form
-
+export default Form;
--- pages/recipes/feed.jsx
+++ pages/recipes/feed.jsx
@@ -1,41 +1,42 @@
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Typography from '@material-ui/core/Typography';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import RecipeCard from '../../component/RecipeCard';
-import styles from '../../styles/feed.module.css';
-
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Typography from "@material-ui/core/Typography";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import RecipeCard from "../../component/RecipeCard";
+import styles from "../../styles/feed.module.css";
 
 const Feed = () => {
-    
-    const [recipes, setRecipes] = useState([])
-    const [error, setError] = useState(false)
-    const [fetchedRecipes, setFetchedRecipes] = useState(false)
+	const [recipes, setRecipes] = useState([]);
+	const [error, setError] = useState(false);
+	const [fetchedRecipes, setFetchedRecipes] = useState(false);
 
-    useEffect(() => {
-        axios.get('https://super-supper.ue.r.appspot.com/recipes', 
-        ).then(res => setRecipes(res.data))
-        .catch(err => console.log(err))
-        .finally(setFetchedRecipes(true))
-      }, [])
+	useEffect(() => {
+		axios
+			.get("https://super-supper.ue.r.appspot.com/recipes")
+			.then((res) => {
+				console.log(res.data);
+				setRecipes(res.data);
+			})
+			.catch((err) => console.log(err))
+			.finally(setFetchedRecipes(true));
+	}, []);
 
+	return (
+		<div className={styles.container}>
+			<div className={styles.header}>
+				<Typography variant="h2" component="h2">
+					Recipe Feed
+				</Typography>
+			</div>
+			<div className={styles.feed}>
+				{!fetchedRecipes ? (
+					<LinearProgress />
+				) : (
+					recipes.map((item) => <RecipeCard key={item._id} recipe={item} />)
+				)}
+			</div>
+		</div>
+	);
+};
 
-    return (
-    <div className={styles.container}>
-        <div className={styles.header}>
-         <Typography variant="h2" component="h2">
-         Recipe Feed
-        </Typography>
-        </div>
-        <div className={styles.feed}>
-            {!fetchedRecipes ? 
-            <LinearProgress /> :
-            recipes.map(item => <RecipeCard key={item.id} recipe={item} />)
-            }
-        </div>
-    </div>
-    )
-
-}
-
-export default Feed
+export default Feed;
